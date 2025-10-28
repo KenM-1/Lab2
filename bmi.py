5e5fb23d59d422e81fc793564d4d9405df285768
Kenneth <KLIAUW.24@ichat.sp.edu.sg>
2025-10-28 15:30:30 +0800

    Initial version to display text message on console

---
+++ bmi.py
@@ -0,0 +1,9 @@
+def calculate_bmi(height, weight): 
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height * height)
+    print("bmi = " + str(bmi))
+    
+calculate_bmi(weight= 57, height = 1.73)
+
+
