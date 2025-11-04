892a4303b1ec1a207b92ae0a4f5b9af642c6e76f
Kenneth <KLIAUW.24@ichat.sp.edu.sg>
2025-11-04 14:49:28 +0800

    Initial version to display text message on console

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,12 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def get_user_input(): 
+    print("get_user_input")
+    user_input = input("Enter temperature values: ").split(",")
+    floatLst = []
+    for x in user_input:
+        floatNumber = float(x)
+        floatLst.append(floatNumber)
+
+    print(floatLst)
+    return floatLst
+
+blah = get_user_input()
--- bmi.py
+++ bmi.py
@@ -5,11 +5,11 @@ def calculate_bmi(height, weight):
     print("bmi = " + str(bmi))
 
     if bmi <= 18.5:
-        print("Underweight")
+        print("-1")
     elif 18.5 <= bmi <= 25.0:
-        print("Normal Weight")
+        print("0")
     else:
-        print("Overweight")
+        print("1")
     
 calculate_bmi(weight= 57, height = 1.73)
 
